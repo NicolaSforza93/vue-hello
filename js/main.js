1a36e420d1257e6093070eb60f1ddce502443d12
NicolaSforza93 <nicola.sforza93@gmail.com>
2023-10-23 16:47:57 +0200

    added Bonus

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
 createApp({
 data() {
     return {
-    message: 'Hello Vue!'
+    message: 'Hello Vue!',
+    imgSrc: 'https://cdn.sanity.io/images/8edntncj/production/7cb22a8a9885468d96662f6ece61ceb2bc95666d-500x300.png',
     }
 }
 }).mount('#app')
