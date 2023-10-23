2676919aebc3d0c9314813b2609a43c63b500a71
NicolaSforza93 <nicola.sforza93@gmail.com>
2023-10-23 19:12:45 +0200

    added tests

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 const { createApp } = Vue
 
 createApp({
-data() {
-    return {
-    message: 'Hello Vue!',
-    imgSrc: 'https://cdn.sanity.io/images/8edntncj/production/7cb22a8a9885468d96662f6ece61ceb2bc95666d-500x300.png',
+    data() {
+        return {
+            message: 'Hello Vue!',
+            imgSrc: 'https://cdn.sanity.io/images/8edntncj/production/7cb22a8a9885468d96662f6ece61ceb2bc95666d-500x300.png',
+            mieClassi: 'title',
+            inputValue: 'ciao Vue!',
+            num: 0,
+        }
+    },
+    methods: {
+        onClick: function () {
+            // console.log(this.num);
+            this.num++;
+        }
     }
-}
 }).mount('#app')
